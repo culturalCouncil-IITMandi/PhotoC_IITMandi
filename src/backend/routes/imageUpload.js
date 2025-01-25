84417dda06d0eb72c59d3b231e6653f9fa7cc367
Ojasvi310 <ojasvisj@gmail.com>
2025-01-26 01:13:21 +0530

    Filtering and sorting

--- src/backend/routes/imageUpload.js
+++ src/backend/routes/imageUpload.js
@@ -1,8 +1,10 @@
-import express from 'express'
+import express from 'express';
+import { imageFilters } from '../controllers/imageAccess.js';
 
 import { uploadImages } from '../controllers/imageUpload.js'
 
 const router = express.Router();
 router.post('/upload', uploadImages); // REMINDER: Add auth middleware here
+router.get('/images', imageFilters);
 
 export default router;
